4d345c8243269fec9d361f7159c4b2611bb5bea6
joansusu <joanxu2023@gmail.com>
2025-04-23 16:31:15 +0800

    GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             </p>
           </div>
         </footer>
+        <GoogleAnalytics gaId="G-FDHPQH84XC" />
       </body>
     </html>
   );
